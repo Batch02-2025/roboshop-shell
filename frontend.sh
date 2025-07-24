0c860125064d99b8a6b4f3fc20733e67f60c3b68
MannuA19 <mannmeet.ahir@gmail.com>
2025-07-24 19:42:35 +0530

    Roboshop Shell-Script Files

--- frontend.sh
+++ frontend.sh
@@ -9,3 +9,14 @@ sudo dnf install nginx -y
 sudo systemctl enable nginx
 sudo systemctl start nginx
 
+# Remove the default content that web server is serving
+sudo rm -rf /usr/share/nginx/html/*
+
+# Download the frontend content
+sudo curl -o /tmp/frontend.zip https://roboshopartifacts.s3.amazonaws.com/frontend-v3.zip
+
+# Extract the frontend content
+sudo cd /usr/share/nginx/html
+sudo unzip /tmp/frontend.zip
+
+# Nginx Reverse Proxy Configuration

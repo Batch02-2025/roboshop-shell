813423f06085e47617f7122f0f9ff6df9987f1fe
MannuA19 <mannmeet.ahir@gmail.com>
2025-07-25 18:27:36 +0530

    Roboshop Shell-Script file

--- frontend.sh
+++ frontend.sh
@@ -1,22 +1,35 @@
 #! /bin/bash
 
-# Install Nginx
+echo "==== Disable Nginx Module ===="
 sudo dnf module disable nginx -y
+echo "Exit status: $?"
+
+echo "=== Enable Nginx Module 1.24 ===="
 sudo dnf module enable nginx:1.24 -y
+echo "Exit status: $?"
+
+echo "===== Install Nginx ====="
 sudo dnf install nginx -y
+echo "Exit status: $?"
 
-# Remove the default content that web server is serving
+echo "===== Remove the default content that web server is serving ====="
 sudo rm -rf /usr/share/nginx/html/*
+echo "Exit status: $?"
 
-# Download the frontend content
+echo "===== Download the frontend content ====="
 sudo curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+echo "Exit status: $?"
 
-# Extract the frontend content
+echo "===== Extract the frontend content ====="
 sudo unzip /tmp/frontend.zip -d /usr/share/nginx/html
+echo "Exit status: $?"
 
-# Nginx Reverse Proxy Configuration
+echo "===== Nginx Reverse Proxy Configuration ====="
 cp nginx.conf /etc/nginx/nginx.conf
+echo "Exit status: $?"
 
-# Start and Enable Nginx Service
+echo "===== Start and Enable Nginx Service ====="
 sudo systemctl enable nginx
+echo "Exit status: $?"
 sudo systemctl start nginx
+echo "Exit status: $?"
